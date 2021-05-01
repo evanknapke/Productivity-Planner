b3e23cba74980f94db3f5990345e2d9090bfd21d
Evan Knapke <evanknapke@gmail.com>
2021-05-01 17:07:39 -0400

    Set up event creation template

--- lib/main.dart
+++ lib/main.dart
@@ -10,40 +10,76 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Startup Name Generator',
-      theme: ThemeData(primaryColor: Colors.blue),
-      home: RandomWords(),
+        title: 'Startup Name Generator',
+        theme: ThemeData(primaryColor: Colors.blue),
+        // home: MainApp(),
+        initialRoute: '/',
+        routes: {
+          '/': (context) => MainApp(),
+          '/events_screen': (context) => EventScreen()
+        });
+  }
+}
+
+class EventScreen extends StatefulWidget {
+  @override
+  _EventScreenState createState() => _EventScreenState();
+}
+
+class _EventScreenState extends State<EventScreen> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Create an event")),
+      body: Form(
+        key: _formKey,
+        child: Column(
+          children: <Widget>[
+            TextFormField(
+              // The validator receives the text that the user has entered.
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Validate returns true if the form is valid, or false otherwise.
+                if (_formKey.currentState.validate()) {
+                  // If the form is valid, display a snackbar. In the real world,
+                  // you'd often call a server or save the information in a database.
+                  ScaffoldMessenger.of(context)
+                      .showSnackBar(SnackBar(content: Text('Processing Data')));
+                }
+              },
+              child: Text('Submit'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
 
 // shortcut to generate: stful
-class RandomWords extends StatefulWidget {
+class MainApp extends StatefulWidget {
   @override
-  _RandomWordsState createState() => _RandomWordsState();
+  _MainAppState createState() => _MainAppState();
 }
 
-class _RandomWordsState extends State<RandomWords> {
+class _MainAppState extends State<MainApp> {
   int _selectedIndex = 0;
   static const TextStyle optionStyle =
       TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
   static const List<Widget> _widgetOptions = <Widget>[
-    Text(
-      'Index 0: Schedule',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 1: Statistics',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 2: Action Items',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 3: Settings',
-      style: optionStyle,
-    ),
+    SchedulePage(optionStyle: optionStyle),
+    StatisticsPage(optionStyle: optionStyle),
+    ActionsPage(optionStyle: optionStyle),
+    SettingsPage(optionStyle: optionStyle),
   ];
 
   void _onItemTapped(int index) {
@@ -56,7 +92,7 @@ class _RandomWordsState extends State<RandomWords> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('BottomNaviagationBar Sample'),
+        title: const Text('Planner'),
       ),
       body: Center(
         child: _widgetOptions.elementAt(_selectedIndex),
@@ -73,7 +109,7 @@ class _RandomWordsState extends State<RandomWords> {
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.circle),
-            label: 'Action Items',
+            label: 'Tasks',
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.settings),
@@ -85,6 +121,82 @@ class _RandomWordsState extends State<RandomWords> {
         unselectedItemColor: Colors.grey,
         onTap: _onItemTapped,
       ),
+    );
+  }
+}
+
+class SchedulePage extends StatelessWidget {
+  const SchedulePage({
+    Key key,
+    @required this.optionStyle,
+  }) : super(key: key);
+
+  final TextStyle optionStyle;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: 100,
+        child: Center(
+            child: Card(
+                color: Color(0xffe0e0e0),
+                child: InkWell(
+                  splashColor: Colors.blue.withAlpha(30),
+                  onTap: () {
+                    Navigator.pushNamed(context, '/events_screen');
+                  },
+                  child: const Center(child: Text('No Event Added')),
+                ))));
+  }
+}
+
+class StatisticsPage extends StatelessWidget {
+  const StatisticsPage({
+    Key key,
+    @required this.optionStyle,
+  }) : super(key: key);
+
+  final TextStyle optionStyle;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'Index 1: Statistics',
+      style: optionStyle,
+    );
+  }
+}
+
+class ActionsPage extends StatelessWidget {
+  const ActionsPage({
+    Key key,
+    @required this.optionStyle,
+  }) : super(key: key);
+
+  final TextStyle optionStyle;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'Index 2: Tasks',
+      style: optionStyle,
+    );
+  }
+}
+
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({
+    Key key,
+    @required this.optionStyle,
+  }) : super(key: key);
+
+  final TextStyle optionStyle;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'Index 3: Settings',
+      style: optionStyle,
     );
   }
 }
